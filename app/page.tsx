7f640e2905ea77be28e3bc166a0e63f493e0128c
Matteo Bertamini <matteo@bertamini.net>
2023-08-29 01:25:46 +0200

    new landing page with tailwind PoC

--- app/page.tsx
+++ app/page.tsx
@@ -1,117 +1,68 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        {/*<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">*/}
-        {/*  Get started by editing&nbsp;*/}
-        {/*  <code className="font-mono font-bold">app/page.tsx</code>*/}
-        {/*</p>*/}
-        <div className=" fixed bottom-0 left-0 flex h-25 w-full bg-slate-50/70 justify-center">
-          <a
-            className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://www.bertamini.net"
-          >
-            By{' '}
-            <Image
-              src="https://www.bertamini.net/smiley-face.svg"
-              alt="Vercel Logo"
-              className="white:invert"
-              width={24}
-              height={24}
-              priority
-            />
-            bertamini.net
-          </a>
+    return(
+        <>
+        <div className="bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5">
+            <div
+                className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-3"
+            >
+                <div className="xl:max-w-xl">
+                    <img className="h-10" src="/img/logo.svg" alt="GMadrid Natación logo" />
+                    <img
+                        className="rounded-lg drop-shadow-xl h-60 sm:h-96 w-full  object-cover object-top lg:hidden"
+                        src="/img/demo.png"
+                        alt="Pantallazo de la aplicación"
+                    />
+                    <h1
+                        className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl"
+                    >
+                        La App de GMadrid Natación.
+                        <br />
+                        <span className="text-blue-500">Al tanto en un ¡zas!</span>
+                    </h1>
+                    <p className="mt-2 text-gray-600 sm:mt-4 sm:text-xl">
+                        Entrenos, calendario, estado de las piscinas... a partir de hoy olvídate de líos.
+                    </p>
+                    <p className="mt-2 text-gray-600 sm:mt-1 sm:text-xl">
+                        Con GMadrid Natación App podrás estar al tanto de todo lo que necesitas, para ser parte activa de GMadrid Natación.
+                    </p>
+                    <div className="mt-4 sm:mt-6">
+                        <a
+                            className="inline-block px-5 py-3 rounded-lg transform transition bg-blue-500 hover:bg-indigo-400 hover:-translate-y-0.5 focus:ring-indigo-500 focus:ring-opacity-50 focus:outline-none focus:ring focus:ring-offset-2 active:bg-indigo-600 uppercase tracking-wider font-semibold text-sm text-white shadow-lg sm:text-base"
+                            href="#"
+                        >
+                            Instalar
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="hidden relative lg:block 2xl:col-span-2">
+                <div className="absolute mt-10 ml-8 h-full w-60 -inset-0 rounded-lg bg-gradient-radial from-blue-500 via-blue-500 to-transparent opacity-30 blur transition duration-100 "></div>
+                <img
+                    className="absolute w-72 mb-6 object-center "
+                    src="/img/demo.png"
+                    alt="Pantallazo de la aplicación"
+                />
+            </div>
         </div>
-      </div>
-      <div className="w-full text-center">
-          <h1 className="text-3xl opacity-70 justify-center">GMadrid Natación App</h1>
-      </div>
-        {/*<div className="relative flex place-items-center ">*/}
-        {/*    <p>toda información en el palmo de tu mano</p>*/}
-        {/*</div>*/}
-      <div className="flex place-items-center before:absolute before:h-[250px] before:w-[380px] before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[480px] after:w-[180px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] white:invert"
-          src="/demo.png"
-          alt="App"
-          width={250}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://www.bertamini.net"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Instalación{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instala la aplicación en tu dispositivo móvil
-          </p>
-        </a>
-
-        {/*<a*/}
-        {/*  href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"*/}
-        {/*  className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  <h2 className={`mb-3 text-2xl font-semibold`}>*/}
-        {/*    Learn{' '}*/}
-        {/*    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">*/}
-        {/*      -&gt;*/}
-        {/*    </span>*/}
-        {/*  </h2>*/}
-        {/*  <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>*/}
-        {/*    Learn about Next.js in an interactive course with&nbsp;quizzes!*/}
-        {/*  </p>*/}
-        {/*</a>*/}
-
-        {/*<a*/}
-        {/*  href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"*/}
-        {/*  className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  <h2 className={`mb-3 text-2xl font-semibold`}>*/}
-        {/*    Templates{' '}*/}
-        {/*    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">*/}
-        {/*      -&gt;*/}
-        {/*    </span>*/}
-        {/*  </h2>*/}
-        {/*  <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>*/}
-        {/*    Explore the Next.js 13 playground.*/}
-        {/*  </p>*/}
-        {/*</a>*/}
-
-        {/*<a*/}
-        {/*  href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"*/}
-        {/*  className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  <h2 className={`mb-3 text-2xl font-semibold`}>*/}
-        {/*    Deploy{' '}*/}
-        {/*    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">*/}
-        {/*      -&gt;*/}
-        {/*    </span>*/}
-        {/*  </h2>*/}
-        {/*  <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>*/}
-        {/*    Instantly deploy your Next.js site to a shareable URL with Vercel.*/}
-        {/*  </p>*/}
-        {/*</a>*/}
-      </div>
-    </main>
-  )
+          <div className="fixed bottom-0 left-0 flex h-25 w-full bg-slate-50/70 justify-center">
+            <a
+              className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-4"
+              href="https://www.bertamini.net"
+            >
+              By{' '}
+              <Image
+                src="https://www.bertamini.net/smiley-face.svg"
+                alt="Vercel Logo"
+                className="white:invert"
+                width={24}
+                height={24}
+                priority
+              />
+              bertamini.net
+            </a>
+          </div>
+        </>
+);
 }
